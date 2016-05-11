c93a1968b528cfa49b273f99dbdff1869ab0affc
Tobias Wermuth <tobynextdoor@gmail.com>
2016-05-11 19:00:20 +0200

    improve ads

--- Assets/Scripts/AdScript.cs
+++ Assets/Scripts/AdScript.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
+using AppodealAds.Unity;
 using AppodealAds.Unity.Api;
 using AppodealAds.Unity.Common;
 
@@ -10,6 +11,10 @@ public class AdScript : MonoBehaviour {
 	private String APPODEALS_APP_KEY = "220b434377fdc6264f1a2fa999d86f3147344bc982adb219";
 
 	void Start() {
+		UserSettings userSettings = new UserSettings ();
+		userSettings.setAge (16);
+		userSettings.setInterests ("games, friends");
+
 		Appodeal.disableLocationPermissionCheck();
 		Appodeal.initialize(APPODEALS_APP_KEY, Appodeal.INTERSTITIAL | Appodeal.BANNER);
 		Appodeal.show(Appodeal.BANNER_TOP);
