2b4aa401f55abe08ebb664cafe8d149ab106ebad
Tobias Wermuth <tobynextdoor@gmail.com>
2016-05-13 11:28:41 +0200

    fix ads for adnorid

--- Assets/Scripts/AdScript.cs
+++ Assets/Scripts/AdScript.cs
@@ -8,7 +8,12 @@ using AppodealAds.Unity.Api;
 using AppodealAds.Unity.Common;
 
 public class AdScript : MonoBehaviour {
+	#if UNITY_IPHONE
 	private String APPODEALS_APP_KEY = "220b434377fdc6264f1a2fa999d86f3147344bc982adb219";
+	#endif
+	#if UNITY_ANDROID
+	private String APPODEALS_APP_KEY = "009d3f40955fd2fb62a1463daad14e496a76a3c0a85d74c3";
+	#endif
 
 	void Start() {
 		UserSettings userSettings = new UserSettings ();
